979c1cc06bc9378c60f7d7db0e54ce0e0a14c811
akibr <akibr@DESKTOP-J5FBATK>
2020-08-25 16:44:27 -0400

    Working website

--- routes.py
+++ routes.py
@@ -1,33 +1,36 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort,url_for
 import os
 from flask_sqlalchemy import SQLAlchemy
-
-
-#from flask_login import login_user, current_user, logout_user, login_required #Optional login imports, need to 'pip install flask_login'
-
-
 # import boto3
-# from botocore.client import Config
+
+# #from flask_login import login_user, current_user, logout_user, login_required #Optional login imports, need to 'pip install flask_login'
+
+
+
+
 
 # # Get the service client with sigv4 configured
-# s3 = boto3.client('s3', config=Config(signature_version='s3v4') ,    aws_access_key_id="AKIAJTRZTPK26SP27X6Q",
-#     aws_secret_access_key="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")
+# s3 = boto3.client('s3',    aws_access_key_id="AKIAJTRZTPK26SP27X6Q",aws_secret_access_key="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")
 
 # # Generate the URL to get 'key-name' from 'bucket-name'
 # # URL expires in 604800 seconds (seven days)
-# url = s3.generate_presigned_url(
-#     ClientMethod='get_object',
-#     Params={
-#         'Bucket': 'books202',
-#         'Key': 'Books-20200623T203822Z-002/Books/Artemis Fowl/01 artemis fowl - eoin colfer.pdf'
-#     },
-#     ExpiresIn=604800
-# )
+
+# url = s3.generate_presigned_url(ClientMethod='get_object',Params={'Bucket': 'books2020', 'Key': 'Books-20200623T203822Z-002/Books/BrandonSanderson.works/The Stormlight Archieve/Words of Radiance by Brandon Sanderson/words of radiance by brandon sanderson.pdf'  }, ExpiresIn=604800)
 
 
 
+import boto3
 
+books=[]
+s3 = boto3.resource('s3',aws_access_key_id="AKIAJTRZTPK26SP27X6Q", aws_secret_access_key="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")
+s3Client = boto3.client('s3',aws_access_key_id="AKIAJTRZTPK26SP27X6Q",aws_secret_access_key="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")
 
+my_bucket = s3.Bucket('books2020')
+
+for my_bucket_object in my_bucket.objects.all():
+    if ".pdf" in my_bucket_object.key:
+        url = s3Client.generate_presigned_url(ClientMethod='get_object',Params={'Bucket': 'books2020', 'Key': my_bucket_object.key  }, ExpiresIn=604800)
+        books.append({"name":my_bucket_object.key,"url":url})
 
 app = Flask(__name__)
 #postgres://rccubcqkcflzgf:9586b06abbbb73023074a4be39063f07daa68fc3ca77f054941712737ed9809d@ec2-52-87-58-157.compute-1.amazonaws.com:5432/df76b2kmkiedk3
@@ -36,14 +39,22 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 
-books = [{"name" : "Artemis Fowl Book 1",
-        "url" : "https://books2020.s3.amazonaws.com/Books-20200623T203822Z-002/Books/Artemis+Fowl/01+artemis+fowl+-+eoin+colfer.pdf"},
-        {"name" : "Artemis Fowl Book 2",
-        "url" : "https://books2020.s3.amazonaws.com/Books-20200623T203822Z-002/Books/Artemis+Fowl/02+the+arctic+incident+-+eoin+colfer.pdf"},
-        {"name" : "Artemis Fowl Book 3",
-        "url" : "https://books2020.s3.amazonaws.com/Books-20200623T203822Z-002/Books/Artemis+Fowl/04+the+opal+deception+-+eoin+colfer.pdf"}
-        
-        ]
+
+#id | book_name | series_name | author_name
+
+'''
+Display book by Series Name:
+Artemis Fowl
+    ->artermis fowl book 1
+    ->artermis fowl book 2
+    ->artermis fowl book 3
+Mistborn
+    ->mistborn book1
+    ->mistborn book2
+    ->mistborn book3
+Words 
+'''
+
 class Books(db.Model):
     __tablename__ = 'books'
 
@@ -72,7 +83,7 @@ def index():
 def read(book_url):
     awsUrl = books[int(book_url)]["url"]
     print(awsUrl)
-    return render_template("simpleviewer.html",awsUrl = awsUrl)
+    return render_template("mobile-viewer.html",awsUrl = awsUrl)
     
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
